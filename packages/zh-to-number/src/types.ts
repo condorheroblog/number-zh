03267d88c0495ccba6ea70e6ac25053a080c0d8a
CondorHero <love2xinwei@gmail.com>
2023-08-22 17:20:23 +0800

    chore: Modify Build Command

--- packages/zh-to-number/src/types.ts
+++ packages/zh-to-number/src/types.ts
@@ -1,5 +1,7 @@
 import type { LanguageType, resourcesType } from "../../number-to-zh/src";
 
+export type { LanguageType, resourcesType };
+
 export interface ZhToNumberOptions {
 	language?: LanguageType;
 	resources?: resourcesType;
