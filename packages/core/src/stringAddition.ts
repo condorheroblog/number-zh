ec9216f77db2e054b1cebccdfe4983da178889c1
CondorHero <love2xinwei@gmail.com>
2023-08-22 13:52:28 +0800

    chore: use BigInt replace custom plus

--- packages/core/src/stringAddition.ts
+++ packages/core/src/stringAddition.ts
@@ -1,28 +1,6 @@
 export function stringAddition(string1: string, string2: string) {
-	const maxLength = Math.max(string1.length, string2.length);
-	const paddedString1 = string1.padStart(maxLength, "0");
-	const paddedString2 = string2.padStart(maxLength, "0");
+	const bigInt1 = BigInt(string1);
+	const bigInt2 = BigInt(string2);
 
-	let result = "";
-	// 进位
-	let carry = 0;
-
-	for (let i = maxLength - 1; i >= 0; i--) {
-		const digit1 = parseInt(paddedString1[i]);
-		const digit2 = parseInt(paddedString2[i]);
-
-		const sum = digit1 + digit2 + carry;
-		// 当前位的结果
-		const digitSum = sum % 10;
-		// 计算进位
-		carry = Math.floor(sum / 10);
-
-		result = digitSum.toString() + result;
-	}
-
-	if (carry > 0) {
-		result = carry.toString() + result;
-	}
-
-	return result;
+	return (bigInt1 + bigInt2).toString();
 }
